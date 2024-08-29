04f2ae41844e801a020d07d8feb3e99e0ae02ea7
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-08-29 10:04:37 -0700

    Update interview_preparer.py
    Add comment

--- agents/interview_preparer.py
+++ agents/interview_preparer.py
@@ -1,13 +1,5 @@
-# from crewai import Agent
-# from crewai_tools import ScrapeWebsiteTool, SerperDevTool, PDFSearchTool
-# from langchain.tools import DuckDuckGoSearchRun
-# from utils.pdf_tool import PDFReadTool
+#An agent to create potential interview questions and talking points taking candidate resume into account
 
-# search_tool = DuckDuckGoSearchRun() 
-# search_tool_serp = SerperDevTool()
-# scrape_tool = ScrapeWebsiteTool()
-# semantic_search_resume = PDFSearchTool()
-# pdf_tool = PDFReadTool()
 from crewai import Agent
 from utils.tools import scrape_tool, search_tool, pdf_tool, semantic_search_resume
 
